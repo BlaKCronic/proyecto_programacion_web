db01884abaa27cb874a54cd5d438c97eae22ac89
BlaKCronic <poncegonzalez849@gmail.com>
2025-10-04 11:06:56 -0600

    ajuste de la conexion a la base de datos

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+class DataBase{
+    var $_DNS = "mysql:host=mariadb;dbname=database";
+    var $_USER = "user";
+    var $_PASSWORD = "password";
+    var $_BD = null;
+    function conect(){
+        $this -> _BD = new PDO($this -> _DNS, $this -> _USER, $this -> _PASSWORD);
+    }
+}
+?
